0cc5c215873ca07ac9e07241575be201aef15cf9
KenChen <aken112981@gmail.com>
2026-08-27 22:04:10 +0800

    fix delay test

--- core/src/main/java/com/github/kr328/clash/core/model/DelayTestResult.kt
+++ core/src/main/java/com/github/kr328/clash/core/model/DelayTestResult.kt
@@ -1,5 +1,8 @@
 package com.github.kr328.clash.core.model
 
+import android.os.Parcel
+import android.os.Parcelable
+import com.github.kr328.clash.core.util.Parcelizer
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -10,7 +13,20 @@ data class DelayTestResult(
     val failed: Int,
     val elapsedMs: Long,
     val error: String? = null,
-) {
+) : Parcelable {
     val hasSuccess: Boolean
         get() = succeeded > 0
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        Parcelizer.encodeToParcel(serializer(), parcel, this)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<DelayTestResult> {
+        override fun createFromParcel(parcel: Parcel): DelayTestResult =
+            Parcelizer.decodeFromParcel(serializer(), parcel)
+
+        override fun newArray(size: Int): Array<DelayTestResult?> = arrayOfNulls(size)
+    }
 }
